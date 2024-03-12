2ba088c55eaa746d6b8c61c4db0f5b5bfa1d0b5e
Ashik <ashikur1993@gmail.com>
2024-03-13 01:05:28 +0600

    First coment cart running...

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     content: ["*"], // * for all files.
     theme: {
         extend: {},
+        container: {
+            center: true,
+            padding: "2rem",
+        },
     },
     plugins: [],
 };
