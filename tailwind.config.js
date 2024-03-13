5db3a25810dba3d5ebbff644701e015ec91d556d
Ashik <ashikur1993@gmail.com>
2024-03-14 01:18:51 +0600

    Comment cart complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
             center: true,
             padding: "2rem",
         },
+        colors: {
+            primary: "#4D50AD",
+            gray: "#F5F6FA",
+            white: "#ffffff",
+            "dark-1": "#3A434C",
+            "dark-2": "#74777E",
+            "dark-3": "#E8E9ED",
+            "dark-4": "#6C6F74",
+            red: "#FF5252",
+        },
     },
     plugins: [],
 };
